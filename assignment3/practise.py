1d577d2a65ac588ceea9ba98eb084d8e002bbf37
KKotchakorn <52272952+KKotchakorn@users.noreply.github.com>
2019-07-18 09:00:14 +0700

    Update practise.py

--- assignment3/practise.py
+++ assignment3/practise.py
@@ -1,1 +1,11 @@
+my_list = [23,42,35]
+print(my_list)
+print("Sum = ",sum(my_list))
 
+
+my_tuple = (14,32,12,54,65)
+print("Tuple = ",my_tuple)
+
+set1 = {5,11,9,13,12,15,17,77,43,87,65,98}
+set2 = {5,12,71,97,77,9,41,223,413,108,120}
+print("Intersection = ",set1.intersection(set2))
