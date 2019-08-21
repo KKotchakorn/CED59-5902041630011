7e17f2a071dbd75da145f3f7e29dd3512b5bdc42
Kkotchakorn <s5902041630011@email.kmutnb.ac.th>
2019-08-21 20:23:18 +0700

    assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,10 +1,32 @@
+from flask import Flask,render_template , request
+from wtforms import StringField,PasswordField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email
 
-from flask import Flask,render_template
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
 
-@app.route("/")
-def member():
-    return render_template('Register.html')
+class patRegisForm(FlaskForm):
+    Firstname = StringField("FirstName", validators=[InputRequired()])
+    Lastname = StringField("Lastname", validators=[InputRequired()])
+    Email = StringField("Email",  validators=[InputRequired("Please enter your email address."), Email("Please enter your email again.")])
+    Username = StringField("Username", validators=[InputRequired()])
+    Password = PasswordField("Password", validators=[InputRequired(), Length(min=8, message="Please enter your password 8 character.")])
 
-if __name__ == "__main__":
+@app.route('/')
+def student():
+    form = patRegisForm()
+    return render_template('regis.html', form=form)
+
+
+@app.route('/regis', methods=["GET", "POST"])
+def regis():
+    form = patRegisForm()
+    if form.validate_on_submit():
+        return "Ok"
+    return render_template('regis.html', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
